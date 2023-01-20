04f168de3d726e4b5aec63ca9d5dd2e86a215ee2
Sherwin-Roger <sherwinroger02@gmail.com>
2023-01-20 18:32:22 +0530

    Update spt.py

--- spt.py
+++ spt.py
@@ -1,12 +1,29 @@
+import streamlit as st
+import hydralit_components as hc
 import app1
 import app2
-import streamlit as st
 
-PAGES = {
-    "Cheri - AI": app1,
-    "Neytiri - AI": app2
-}
-st.sidebar.title('Navigation')
-selection = st.sidebar.radio("Go to", list(PAGES.keys()))
-page = PAGES[selection]
-page.app()
+st.set_page_config(layout="wide",initial_sidebar_state='collapsed',)
+
+
+menu_data = [
+
+]
+
+over_theme = {'txc_inactive': '#FFFFFF'}
+menu_id = hc.nav_bar(
+    menu_definition=menu_data,
+    override_theme=over_theme,
+    home_name='Cheri-AI',
+    login_name='Neytiri-AI',
+    hide_streamlit_markers=False, 
+    sticky_nav=True,
+    sticky_mode='pinned', 
+)
+
+st.info(f"{menu_id}")
+
+if menu_id=="Cheri-AI":
+    app1.app()
+else:
+    app2.app()
